f8a9772208f4a0c4d1d2f21fc7bfc392ac27116d
PanoramaView <zhou-1997@hotmail.it>
2021-07-21 09:44:41 +0200

    carico img per layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,22 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container" style="color: red">
-    <h1>benvenuto su Deliverboo</h1>
-</div>
-
-{{-- <div class="container">
-    @foreach ($restaurants as $restaurant)
-    <div class="col-4">
-        <h1>{{$restaurant->name}}</h1>
-        <h2>{{$restaurant->address}}</h2>
-    </div>
-    @endforeach
-</div> --}}
+ 
 <div class="flex-center position-ref full-height">
     <div class="flex-center position-ref full-height text-center">
-        <a class="display-4" href="{{ route("restaurants.index") }}">Vai ai ristoranti</a>
+        <a class="display-4" href="{{ route("restaurants.index") }}">Cerca ristoranti</a>
     </div>
 </div>
 @endsection
