6fc33915e5bcb926acb93f6c0be457119aba2ddb
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2021-07-23 16:16:23 +0200

    fixes Show crud Working on Vue link

---
+++ resources/views/partials/modifyBtn.blade.php
@@ -0,0 +1,5 @@
+
+<button type="button" class="btn btn-warning spacing">
+    <a href=" {{ route('admin.restaurants.edit', $restaurant->id) }} ">Modifica</a>
+</button>
+
---
+++ resources/views/partials/showBtn.blade.php
@@ -0,0 +1,3 @@
+<button type="button" class="btn btn-info spacing">
+    <a href=" {{ route('admin.restaurants.show', $restaurant->id) }} ">Visualizza il tuo ristorante</a>
+</button>
