958694c2b7a391b1375b4fad642963ab98686c3a
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2021-07-30 11:41:51 +0200

    fixes seeder function

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -4,5 +4,13 @@
 
 <restaurant-index :types='@json($types)'></restaurant-index>
 
+<div class="pink-bg">
+    <div class="container">
+        <div class="pink-bg-text">
+            <h1>La selezione di Deliveroo</h1>
+        </div>
+    </div>
+</div>
+
 
 @endsection
