637aaa040c7de3b2bdc76bd959919ff35cff3548
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-07-30 10:04:13 +0200

    Aggiunto seeders ristoranti piatti tipologie

--- database/seeds/RestaurantsTableSeeder.php
+++ database/seeds/RestaurantsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Restaurant;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RestaurantsTableSeeder extends Seeder
 {
@@ -17,28 +18,104 @@ class RestaurantsTableSeeder extends Seeder
             DB::table('restaurants')->insert([
 
                 [
-                    'name' => 'Ristorante di Pippo',
-                    'address' => 'Via como 1',
+                    'name' => 'Ristorante al Colosseo',
+                    'address' => 'Via Del Corso 1',
+                    'img_url' => 'restaurants_cover/al_colosseo.webp',
+                    'user_id' => '3',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                 ],
                 [
-                    'name' => 'Ristorante di Pluto',
-                    'address' => 'Via como 2',
+                    'name' => 'Ristorante La Grande Muraglia',
+                    'address' => 'Via Dante Alighieri 8',
+                    'img_url' => 'restaurants_cover/la_grande-muraglia.jpeg',
+                    'user_id' => '4',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                 ],
                 [
-                    'name' => 'Ristorante di Minnie',
-                    'address' => 'Via como 3',
+                    'name' => 'Pizzeria Palermo Bedda',
+                    'address' => 'Via Leonardo Da Vinci 12',
+                    'img_url' => 'restaurants_cover/palermo.jpeg',
+                    'user_id' => '2',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Magic Thailand',
+                    'address' => 'Via Valentino Rossi 46',
+                    'img_url' => 'restaurants_cover/magic_thailand.jpeg',
+                    'user_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Burger King',
+                    'address' => 'Via Alessandro Sainato 21',
+                    'img_url' => 'restaurants_cover/burger_king.jpeg',
+                    'user_id' => '4',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'KFC',
+                    'address' => 'Via Verdi 31',
+                    'img_url' => 'restaurants_cover/kfc.jpeg',
+                    'user_id' => '2',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Il mare a quadretti',
+                    'address' => 'Viale Garibaldi 31',
+                    'img_url' => 'restaurants_cover/il_mare_a_quadretti.png',
+                    'user_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Shibuya',
+                    'address' => 'Porta Palazzo 12',
+                    'img_url' => 'restaurants_cover/shibuya.jpeg',
+                    'user_id' => '3',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Incheon',
+                    'address' => 'Corso Giulio Cesare 320',
+                    'img_url' => 'restaurants_cover/incheon.jpeg',
+                    'user_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Mc Cafè',
+                    'address' => 'Stazione centrale',
+                    'img_url' => 'restaurants_cover/mc_cafe.jpeg',
+                    'user_id' => '2',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Bella Amalfi',
+                    'address' => 'Via Appia Nuova 156',
+                    'img_url' => 'restaurants_cover/bella_amalfi.jpeg',
+                    'user_id' => '4',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Bislakko Pastry',
+                    'address' => 'Corso Vercelli 326',
+                    'img_url' => 'restaurants_cover/bislakko.jpeg',
+                    'user_id' => '3',
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                 ]
-
             ]);
             
         } else { echo "\e[31mTable is not empty, therefore NOT "; }
-
     }
 }
         
---
+++ database/seeds/RestaurantsTypeTableSeeder.php
@@ -0,0 +1,149 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RestaurantsTypeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(DB::table('restaurant_type')->count() == 0){
+
+            DB::table('restaurant_type')->insert([
+
+                [
+                    'restaurant_id' => '1',
+                    'type_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '2',
+                    'type_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '4',
+                    'type_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '7',
+                    'type_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '8',
+                    'type_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '3',
+                    'type_id' => '2',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '1',
+                    'type_id' => '3',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '3',
+                    'type_id' => '3',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '8',
+                    'type_id' => '3',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '5',
+                    'type_id' => '4',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '3',
+                    'type_id' => '4',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '2',
+                    'type_id' => '4',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '7',
+                    'type_id' => '5',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '8',
+                    'type_id' => '6',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '2',
+                    'type_id' => '7',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '4',
+                    'type_id' => '9',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '9',
+                    'type_id' => '8',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '9',
+                    'type_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '10',
+                    'type_id' => '10',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '11',
+                    'type_id' => '1',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'restaurant_id' => '12',
+                    'type_id' => '2',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]
+                ]);
+            
+        } else { echo "\e[31mTable is not empty, therefore NOT "; }
+    }
+}
