20dc103b03c076b7143f90ac3ef36b0b41456879
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2021-08-01 11:24:38 +0200

    npm

--- public/js/app.js
+++ public/js/app.js
@@ -2770,9 +2770,9 @@ __webpack_require__.r(__webpack_exports__);
   // 	}
   // 	return $month_array;
   //     }
-  //   // getMonthlyOrderData() {
-  //   // },
-  // }
+  //   getMonthlyOrderData() {
+  //   },
+  // },
   mounted: function mounted() {
     var _this = this;
 
@@ -52374,14 +52374,15 @@ __webpack_require__.r(__webpack_exports__);
 /*!*****************************************************!*\
   !*** ./resources/js/components/StatisticsIndex.vue ***!
   \*****************************************************/
-/*! exports provided: default */
+/*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _StatisticsIndex_vue_vue_type_template_id_11298254___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./StatisticsIndex.vue?vue&type=template&id=11298254& */ "./resources/js/components/StatisticsIndex.vue?vue&type=template&id=11298254&");
 /* harmony import */ var _StatisticsIndex_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./StatisticsIndex.vue?vue&type=script&lang=js& */ "./resources/js/components/StatisticsIndex.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _StatisticsIndex_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _StatisticsIndex_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -52411,7 +52412,7 @@ component.options.__file = "resources/js/components/StatisticsIndex.vue"
 /*!******************************************************************************!*\
   !*** ./resources/js/components/StatisticsIndex.vue?vue&type=script&lang=js& ***!
   \******************************************************************************/
-/*! exports provided: default */
+/*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
