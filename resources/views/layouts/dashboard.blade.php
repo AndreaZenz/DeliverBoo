906779d3e0b98e5344c2891479767e05957dd465
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-08-03 17:49:37 +0200

    Sidebar responsive

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -32,147 +32,205 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     @yield('librery')
-    
+
     <title>{{ config('app.name', 'DeliveBoo') }}</title>
 </head>
 <body>
+
     <div class="container">
-        <nav class="navbar navbar-expand-md navbar-link bg-white flex-md-nowrap p-0">
-
-
-
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="{{ url('/') }}">
                 <img src="{{ asset('img/loghi/white-logo.png')}}" alt="">
             </a>
+            <button class="navbar-toggler my-navbar-toggler nav-item-ut" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    <button class="navbar-toggler nav-tog-ut" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </ul>
-                
-                {{-- -- --}}
-                <ul class="navbar-nav px-3 ml-auto">
-                    <li class="nav-item-ut">
-                        <a class="nav-link-ut" href="{{route('restaurants.index')}}">
-                            Visita il sito
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0 my-navbar-collapse">
+                    <li>
+                        <a class="nav-link" href="{{route('admin.restaurants.index')}}">
+                            <span>
+                                <i class="fa fa-cutlery responsive-i" aria-hidden="true"></i>
+                            </span>
                         </a>
                     </li>
-                    <li class="nav-item-ut">
+                    <li>
+                        <a class="nav-link" href="{{ route('admin.restaurants.create') }}">
+                            <span>
+                                <i class="fa fa-plus responsive-i" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="{{ route('admin.statistics')}}">
+                            <span>
+                                <i class="fa fa-bar-chart responsive-i" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="#">
+                            <span>
+                                <i class="fa fa-file-text-o responsive-i" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="#">
+                            <span>
+                                <i class="fa fa-cart-plus responsive-i" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="{{ route('admin.types.index')}}">
+                            <span>
+                                <i class="fa fa-tags responsive-i" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
                         <a class="nav-link-ut" href="{{ route('logout') }}" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
-                            Logout
+                            <i class="fa fa-sign-out responsive-i logout-i" aria-hidden="true"></i>
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
                     </li>
                 </ul>
-        </nav>
-        <div class="container-fluid">
-            <div class="row">
-                <nav class="col-md-2 d-none d-md-block bg-light sidebar py-4">
-                    <div class="sidebar-sticky">
-                        <ul class="nav flex-column">
-                            {{-- <li class="nav-item">
-                                <a class="nav-link active" href="{{ route('admin.home_login') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
-                                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                        <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                    </svg>
-                                    Dashboard
-                                </a>
-                            </li> --}}
-
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('admin.restaurants.index')}}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                                        <polyline points="14 2 14 8 20 8"></polyline>
-                                        <line x1="16" y1="13" x2="8" y2="13"></line>
-                                        <line x1="16" y1="17" x2="8" y2="17"></line>
-                                        <polyline points="10 9 9 9 8 9"></polyline>
-                                    </svg>
-                                    I miei ristoranti
-                                </a>
-                            </li>
-
-                            
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.restaurants.create') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                                        <polyline points="14 2 14 8 20 8"></polyline>
-                                        <line x1="16" y1="13" x2="8" y2="13"></line>
-                                        <line x1="16" y1="17" x2="8" y2="17"></line>
-                                        <polyline points="10 9 9 9 8 9"></polyline>
-                                    </svg>
-                                    Aggiungi il tuo ristorante
-                                </a>
-                            </li>
-
-                            
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.statistics')}}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                                        <polyline points="14 2 14 8 20 8"></polyline>
-                                        <line x1="16" y1="13" x2="8" y2="13"></line>
-                                        <line x1="16" y1="17" x2="8" y2="17"></line>
-                                        <polyline points="10 9 9 9 8 9"></polyline>
-                                    </svg>
-                                    Sales
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                                        <polyline points="14 2 14 8 20 8"></polyline>
-                                        <line x1="16" y1="13" x2="8" y2="13"></line>
-                                        <line x1="16" y1="17" x2="8" y2="17"></line>
-                                        <polyline points="10 9 9 9 8 9"></polyline>
-                                    </svg>
-                                    Menu
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                                        <polyline points="14 2 14 8 20 8"></polyline>
-                                        <line x1="16" y1="13" x2="8" y2="13"></line>
-                                        <line x1="16" y1="17" x2="8" y2="17"></line>
-                                        <polyline points="10 9 9 9 8 9"></polyline>
-                                    </svg>
-                                    Ordini
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.types.index')}}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
-                                        <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path>
-                                        <line x1="7" y1="7" x2="7" y2="7"></line>
-                                    </svg>
-                                    Tipologie
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-
-                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 py-4">
-                    <div id="app">
-                        @yield('content')
-                    </div>
-                </main>
 
             </div>
+            <div class="logout-container nav-item-ut">
+                <a class="nav-link-ut" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        </nav>
+        <div class="wrapper">
+            <nav id="sidebar" class="col-md-3">
+                <ul class="list-unstyled components">
+                    <li class="active">
+                        <a class="nav-link" href="{{route('admin.restaurants.index')}}">
+                            <span>
+                                <i class="fa fa-cutlery" aria-hidden="true"></i>
+                                <p>I tuoi ristoranti</p>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="{{ route('admin.restaurants.create') }}">
+                            <span>
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                                <p>Aggiungi un ristorante</p>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="{{ route('admin.statistics')}}">
+                            <span>
+                                <i class="fa fa-bar-chart" aria-hidden="true"></i>
+                                <p>Vendite</p>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="#">
+                            <span>
+                                <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                                <p>Menu</p>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="#">
+                            <span>
+                                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+                                <p>Ordini</p>
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="{{ route('admin.types.index')}}">
+                            <span>
+                                <i class="fa fa-tags" aria-hidden="true"></i>
+                                <p>Tipologie</p>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-9 px-4 py-4">
+                <div id="app">
+                    @yield('content')
+                </div>
+            </main>
         </div>
+
+        {{-- <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-3 d-none d-md-block bg-light sidebar py-4">
+                    <div class="sidebar-sticky">
+                        <ul class="nav flex-column"><li class="nav-item">
+                                <a class="nav-link" href="{{route('admin.restaurants.index')}}">
+        <i class="fa fa-cutlery" aria-hidden="true"></i>
+        I miei ristoranti
+        </a>
+        </li>
+
+
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.restaurants.create') }}">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+                Aggiungi il tuo ristorante
+            </a>
+        </li>
+
+
+
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.statistics')}}">
+                <i class="fa fa-bar-chart" aria-hidden="true"></i>
+                Sales
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                Menu
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+                Ordini
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.types.index')}}">
+                <i class="fa fa-tags" aria-hidden="true"></i>
+                Tipologie
+            </a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-9 px-4 py-4">
+        <div id="app">
+            @yield('content')
+        </div>
+    </main>
+
+    </div>
+    </div> --}}
+
     </div>
     @include('partials.footer')
 </body>
