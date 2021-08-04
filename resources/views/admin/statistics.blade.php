1c4e3686549b33d2cd01f33e3391472745b79148
PanoramaView <zhou-1997@hotmail.it>
2021-08-04 11:45:47 +0200

    Edit div

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -57,7 +57,7 @@
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Target Annuale<br> (<span id='year_target'></span>€)
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Target Anno<br> (<span id='year_target'></span>€)
                         </div>
                         <div class="row no-gutters align-items-center">
                             <div class="col-auto">
